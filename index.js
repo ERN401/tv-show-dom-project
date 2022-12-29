ecd6b10bca0c647af7ec686f12a61c8b89d5cb03
Erfan Riahi <erfanriahi90@gmail.com>
2022-12-29 16:47:32 +0330

    fix title & summary & some responsive thing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fetchAPI = async () => {
   try {
-    const res = await fetch("data.json");
-    /*
-     https://api.tvmaze.com/shows/82/episodes
+    // const res = await fetch("data.json");
+    const res = await fetch("https://api.tvmaze.com/shows/82/episodes");
+    /*     
      freinds: 431
      dark: 17861
      the office: 526
@@ -30,11 +30,11 @@ function show(data) {
     const div = document.createElement("div");
     const img = document.createElement("img");
     const number = document.createElement("p");
+    const rate = document.createElement("span");
+    const runtime = document.createElement("span");
     const title = document.createElement("h3");
     const link = document.createElement("a");
     const summary = document.createElement("p");
-    const rate = document.createElement("span");
-    const runtime = document.createElement("span");
 
     div.style.display = "flex";
     div.style.flexDirection = "column";
@@ -48,52 +48,81 @@ function show(data) {
     if (episode.image) {
       img.src = episode.image.medium;
     }
-    div.append(img);
-    // console.log(episode.image === null);
 
+    //number of season and episode
     const seasonNumber = Number(episode.season).toLocaleString("en-US", {
       minimumIntegerDigits: 2,
     });
     const episodeNumber = Number(episode.number).toLocaleString("en-US", {
       minimumIntegerDigits: 2,
     });
-    number.innerText = `S${seasonNumber}E${episodeNumber}`;
+    const numberOfSeasonAndEpisode = `S${seasonNumber}E${episodeNumber}`;
     number.style.display = "flex";
     number.style.justifyContent = "space-between";
     number.style.padding = "2px";
     number.style.margin = "0";
     number.style.backgroundColor = "#6B7A83";
     number.style.color = "white";
+
+    //rate style
     rate.innerText = `${episode.rating.average}`;
     rate.style.backgroundColor = "#e2b616";
     rate.style.color = "#121212";
     rate.style.padding = "0px 2px";
     rate.style.borderRadius = "2px";
     rate.style.fontWeight = "bold";
-    number.insertAdjacentElement("afterbegin", rate);
-    runtime.innerText = `${episode.runtime}'`;
-    number.append(runtime);
-    div.append(number);
 
-    link.innerText = episode.name;
+    // runtime of the episode
+    runtime.innerText = `${episode.runtime}'`;
+
+    // title of the episode
+    title.innerText = episode.name;
+    title.style.whiteSpace = "nowrap";
+    title.style.overflow = "hidden";
+    title.style.textOverflow = "ellipsis";
     link.href = episode.url;
     link.target = "_blank";
     link.style.textDecoration = "none";
     link.style.color = "black";
-    title.append(link);
-    div.append(title);
+    link.addEventListener("mouseover", () => {
+      title.title = episode.name;
+      title.style.backgroundColor = "#e2b616";
+      title.style.padding = "2px";
+      title.style.borderRadius = "5px";
+    });
+    link.addEventListener("mouseout", () => {
+      title.style.backgroundColor = "#F6F6F5";
+      title.style.padding = "0px";
+    });
 
-    summary.innerHTML = episode.summary;
+    const summayText = PtagToString(episode.summary);
+    summary.innerText =
+      summayText.length > 180
+        ? summayText.split(" ", 25).join(" ") + "..."
+        : summayText;
     summary.style.margin = "0";
-    div.append(summary);
 
+    number.append(rate, numberOfSeasonAndEpisode, runtime);
+    link.append(title);
+    div.append(img, number, link, summary);
     showEpisodes.append(div);
   }
 }
 
+// summary of epidoses have extra things. this function remove all them
+function PtagToString(p) {
+  if (!p) return "";
+  p = p.replaceAll("<p>", "");
+  p = p.replaceAll("</p>", "");
+  p = p.replaceAll("<br>", "");
+  p = p.replaceAll("</br>", "");
+  p = p.replaceAll("<br />", "");
+  return p;
+}
+
 function selectSeasonEpisode(data) {
   const seasons = document.querySelector("#season");
-  const episodes = document.querySelector("#episode");
+  const episodesSelect = document.querySelector("#episode");
 
   //create options for seasons
   let numberOfSeasons = data[data.length - 1].season;
@@ -104,32 +133,35 @@ function selectSeasonEpisode(data) {
     seasons.appendChild(opt);
   }
 
-  let season, episode, filteredData;
+  let season, episode, episodes, filteredData;
   seasons.addEventListener("change", () => {
     season = seasons.value;
     if (season === "All Seasons") {
-      episodes.innerHTML = "<option selected>All Episodes</option>";
+      episodesSelect.innerHTML = "<option selected>All Episodes</option>";
       show(data);
     } else {
-      episode = data.filter((el) => el.season == season).length;
-      episodes.innerHTML = "<option selected>All Episodes</option>";
+      episodes = data.filter((el) => el.season == season);
+      episodesSelect.innerHTML = "<option selected>All Episodes</option>";
       // creat options for episodes
-      for (let i = 1; i <= episode; i++) {
+      for (let episode of episodes) {
         const opt = document.createElement("option");
-        opt.innerText = i;
-        opt.value = i;
-        episodes.append(opt);
+        const episodeNumber = Number(episode.number).toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+        });
+        opt.innerText = `${episodeNumber} - ${episode.name}`;
+        opt.value = episode.name;
+        episodesSelect.append(opt);
       }
       filteredData = data.filter((el) => el.season == season);
       show(filteredData);
     }
 
-    episodes.addEventListener("change", () => {
-      episode = episodes.value;
+    episodesSelect.addEventListener("change", () => {
+      episode = episodesSelect.value;
       if (episode === "All Episodes") show(filteredData);
       else {
         let singleEpisode = data.filter(
-          (el) => el.season == season && el.number == episode
+          (el) => el.season == season && el.name == episode
         );
         show(singleEpisode);
       }
